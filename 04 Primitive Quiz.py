dff19d492c6e114f5174f379b6e09c929c6c73af
casaclanglarsdaniel-ui <casaclanglarsdaniel@gmail.com>
2025-09-19 23:48:10 +0400

    Create 04 Primitive Quiz.py

---
+++ 04 Primitive Quiz.py
@@ -0,0 +1,24 @@
+question = input("What is the capital of France? ")
+if question.lower() == "paris":
+    print("Correct")
+    #Check if variable is correct and prints correct
+    
+
+else:
+    print ("incorrect")
+    #Check if variable is incorrect and prints incorrect
+
+print ('\n')
+print ("Additional Exercise")
+
+question1 = input("What is the capital of Denmark? ")
+if question1.lower() == "copenhagen":
+    print("Correct")
+    #Check if variable is correct and prints correct
+    
+
+else:
+    print ("incorrect, the answer is Copenhangen. \N{grinning face}" )
+    #Check if variable is incorrect and prints incorrect
+
+
